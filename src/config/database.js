2eb7cacb7587e5a871b15c8c94ae3fd1a70a1615
VIJAY A <139609235+Vijay-Hozo@users.noreply.github.com>
2026-08-03 15:01:29 +0530

    Database

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,3 @@
-// import 'dotenv/config';
-
-// import {neon,neonConfig} from '@neondatabase/serverless';
-// import {drizzle} from 'drizzle-orm/neon-http';
-
-// const sql = neon(process.env.DATABASE_URL);
-
-// const db = drizzle(sql);
-
-// export {sql,db};
-
-// import 'dotenv/config';
-
-// import {neon,neonConfig} from '@neondatabase/serverless';
-// import {drizzle} from 'drizzle-orm/neon-http';
-
-// const sql = neon(process.env.DATABASE_URL);
-
-// const db = drizzle(sql);
-
-// export {sql,db};
-
 import 'dotenv/config';
 
 import {neon,neonConfig} from '@neondatabase/serverless';
